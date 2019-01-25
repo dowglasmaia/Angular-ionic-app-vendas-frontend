26158ec6a7989fe931f45926ff61fa00739b7922
Dowglas Maia <dowglasmaia@gmail.com>
2019-01-25 10:07:21 -0200

    Botão de logout no menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthService } from '../services/auth.service';
 
 
 
@@ -16,13 +17,18 @@ export class MyApp {
 
   pages: Array<{title: string, component: string}>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(
+    public platform: Platform, 
+    public statusBar: StatusBar, 
+    public splashScreen: SplashScreen,
+    public auth: AuthService) {
     this.initializeApp();
 
     // Itens de Menu
     this.pages = [
       { title: 'Profile', component: 'ProfilePage' },
       { title: 'Categorias', component: 'CategoriasPage' },
+      { title: 'Logout', component: ''}
       
     ];
 
@@ -35,7 +41,17 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
-    this.nav.setRoot(page.component);
+  openPage(page: {title:string, component: string}) {
+
+    //verificando se o iten clicado foi o logout.
+    switch (page.title) {
+      case 'Logout':
+      this.auth.logout();
+      this.nav.setRoot('HomePage');
+      break;
+
+      default: this.nav.setRoot(page.component);
+
+    }    
   }
 }
